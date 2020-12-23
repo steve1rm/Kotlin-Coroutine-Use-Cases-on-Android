556ba9a91497e17996d253c8690ff7e4bf09ae3e
Stephen Mason <stevekitkat@gmail.com>
2020-12-23 22:40:28 +0700

    Implemented coroutines

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,13 +1,27 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
+        viewModelScope.launch {
+            try {
+                uiState.value = UiState.Loading
 
+                val listOfAndroidVersions = mockApi.getRecentAndroidVersions()
+                val androidFeature = mockApi.getAndroidVersionFeatures(listOfAndroidVersions.last().apiLevel)
+
+                uiState.value = UiState.Success(androidFeature)
+            }
+            catch(exception: Exception) {
+                uiState.value = UiState.Error(exception.localizedMessage ?: "")
+            }
+        }
     }
 }
