285978a64c5778c63157952b4353ca5c5086ab79
Stephen Mason <stevekitkat@gmail.com>
2021-01-02 20:38:18 +0700

    Adds usecase 8 room

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase8/RoomAndCoroutinesViewModel.kt
@@ -1,7 +1,10 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase8
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RoomAndCoroutinesViewModel(
     private val api: MockApi,
@@ -9,11 +12,44 @@ class RoomAndCoroutinesViewModel(
 ) : BaseViewModel<UiState>() {
 
     fun loadData() {
+        viewModelScope.launch {
+            try {
+                uiState.value = UiState.Loading.LoadFromDb
 
+                val listOfAndroidVersions = database.getAndroidVersions()
+                if (listOfAndroidVersions.isEmpty()) {
+                    uiState.value = UiState.Error(DataSource.DATABASE, "Database is empty")
+                }
+                else {
+                    uiState.value = UiState.Success(
+                        DataSource.DATABASE,
+                        listOfAndroidVersions.mapToUiModelList()
+                    )
+                }
+
+                uiState.value = UiState.Loading.LoadFromNetwork
+
+                val listOfAndroidVersionsFromApi = api.getRecentAndroidVersions()
+                listOfAndroidVersionsFromApi.map { androidVersion ->
+                    database.insert(androidVersion.mapToEntity())
+                }
+
+                uiState.value = UiState.Success(
+                    DataSource.NETWORK,
+                    listOfAndroidVersionsFromApi
+                )
+            }
+            catch(exception: Exception) {
+                Timber.e(exception.localizedMessage)
+                uiState.value = UiState.Error(DataSource.DATABASE, exception.localizedMessage ?: "Unknown")
+            }
+        }
     }
 
     fun clearDatabase() {
-
+        viewModelScope.launch {
+            database.clear()
+        }
     }
 }
 
